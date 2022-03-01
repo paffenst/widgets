13e83a7e469d02b384dc3128053d9751b6b75f78
paffen <pavel.stoyanov0@hm.edu>
2022-03-01 17:31:41 +0100

    style and otheres

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import Accordion from './components/Accordion';
 
-
+const items = [
+    {
+        title: "What ist React?",
+        content: "React is a front end Javascript framework"
+    }, {
+        title: "Why use React? ",
+        content: "React is a favorite JS libraray amony enginners "
+    }, {
+        title: "How do you use React? ",
+        content: "You use React by creating components "
+    }
+];
 export default () => {
-    return <h1>React Widget</h1>
-}; 
+
+    return (
+        <div>
+            <Accordion items={items} />
+        </div>
+    )
+};
