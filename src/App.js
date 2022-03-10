7ef13a7668614a540e3738bf88a242e92d248e54
paffen <pavel.stoyanov0@hm.edu>
2022-03-10 17:37:42 +0100

    add a Dropdown component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
     {
@@ -16,9 +17,26 @@ const items = [
 ];
 export default () => {
 
+    const options = [
+        {
+            label: 'The Color Red',
+            value: 'red'
+        },
+        {
+            label: 'The Color Green',
+            value: 'green'
+        },
+        {
+            label: 'The Color Blue',
+            value: 'blue'
+        }
+
+    ];
+
+
     return (
         <div>
-            <Search />  
+            <Dropdown options={options} />  
         </div>
     )
 };
