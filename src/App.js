2dce1fd89e7c10f8de9e9db9dabbc938a7c89e43
paffen <pavel.stoyanov0@hm.edu>
2022-07-24 23:04:24 +0200

    done lesson 205

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
 import Route from './components/Route';
+import Header from './components/Header';
+
 
 const items = [
     {
@@ -35,34 +37,12 @@ const items = [
         }
     ];
 
-    const showAccordion = () => {
-        if (window.location.pathname === '/') {
-            return <Accordion items={items} />;
-        }
-    }
-
-    const showList = () => {
-        if (window.location.pathname === '/list') {
-            return <Search />;
-        }
-    }
-    const showDropdown = () => {
-        if (window.location.pathname === '/dropdown') {
-            return <Dropdown />;
-        }
-    }
-
-    const showTranslate = () => {
-        if (window.location.pathname === '/translate') {
-            return <Translate />;
-        }
-    }
-
     export default () => {
         const [selected,setSelected] = useState(options[0]);
         const [showDropDown,setShowDropDown] = useState(true);
          return (
         <div>
+        <Header/>
          <Route path = "/" >
             <Accordion items={items} />   
          </Route>
